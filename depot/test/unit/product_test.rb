31703b026350fc59a14428f1d45e05eb8ba3fd2c
sprite728 <cchuang@server>
2012-06-29 14:47:04 -0400

    Validation!

--- depot/test/unit/product_test.rb
+++ depot/test/unit/product_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "the truth" do
+    assert true
+  end
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any? 
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(:title  => "My book title"
+                          :description => "skowpek"
+                          :image_url => "sjweiow.jpg" )
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+      
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+      
+    product.price = 1
+    assert product.invalid?
+  end
+  
 end
