79b913226fc4d5fdafa61a3e6fe77dbd2ba11588
sprite728 <cchuang@server>
2012-07-02 14:03:33 -0400

    Add (Initial) Cart

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120628163237) do
+ActiveRecord::Schema.define(:version => 20120702180005) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
