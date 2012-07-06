6a6175ed2580c044f70fb7d739a661bd211fa751
sprite728 <cchuang@server>
2012-07-06 19:36:27 -0400

    Create loungeboard folder

---
+++ loungeboard/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loungeboard::Application.config.secret_token = '76fc7d7e3f27183033328359b5f18ede8d11173ecad1bb13128cf04f8f97cbd8d0dfe0c2e57fc39ffb274137db07b4f14413d6b73e3dbfa980a4a78027e828dd'
